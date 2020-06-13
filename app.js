3b3c5cf71e09217bd1b61703a84d97ab1c5c4ff1
Aaron <aaron.perez@isita.com.mx>
2020-06-12 19:38:22 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,32 @@
+const argv = require('./config/yargs').argv;
+var colors = require('colors/safe');
+
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado:`, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+}
+
+//let base = '6';
+
+//console.log(process.argv);
+// let arvg2 = process.argv;
+
+// console.log(argv.base);
+
+// let parametro = arvg[2];
+// let base = parametro.split('=')[1];
